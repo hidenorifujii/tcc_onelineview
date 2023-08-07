6384054b9770119a1e33f466dc379ef32654c7b9
Hidenori Fujii <35760609+hidenorifujii@users.noreply.github.com>
2023-08-08 03:48:25 +0900

    layzigit commit

---
+++ footer.js
@@ -0,0 +1,6 @@
+
+`);
+
+const addCss = document.createElement('style');
+addCss.appendChild(css);
+document.head.appendChild(addCss);
---
+++ header.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name TCC One Line View 
+// @description     Compact View
+// @version         0.1
+// @include https://taskchute.cloud/*
+// ==/UserScript==
+
+const css = document.createTextNode(`
+
