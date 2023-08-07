6f0e2c0ff414c3743ec22aa03139df18976966e0
Hidenori Fujii <35760609+hidenorifujii@users.noreply.github.com>
2023-08-08 03:59:17 +0900

    add gitignore files into .gitignore

--- footer.js
+++
@@ -1,6 +0,0 @@
-
-`);
-
-const addCss = document.createElement('style');
-addCss.appendChild(css);
-document.head.appendChild(addCss);
--- header.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name TCC One Line View 
-// @description     Compact View
-// @version         0.1
-// @include https://taskchute.cloud/*
-// ==/UserScript==
-
-const css = document.createTextNode(`
-
